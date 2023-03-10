a19052842a8d1fa6f533e4731e242a8378e7e978
TheRealKwabena <gusasarkw@student.gu.se>
2023-03-10 03:05:39 +0100

    New repositories added for each model and more packages added

---
+++ src/main/java/com/example/university/repositories/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.example.university.repositories;
+
+import com.example.university.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
